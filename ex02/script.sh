3bc371f9ffcf75e752021ca291fca9469c212e7a
Alexandra Bokancha <125573798+AlexandraBokancha@users.noreply.github.com>
2024-03-13 13:25:27 +0100

    Update script.sh

--- ex02/script.sh
+++ ex02/script.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+# “Git unstage filename” removes the file filename from the staging area. #
+# Except being removed from the staging area, no changes are made to the file whatsoever.#
+
 git config --global allias.unstage 'reset HEAD --'
